88f1df8e11c537aabac1d5863774d3c48b1f1727
mjm6635 <mjm6635@208-NWD30.up.ist.local>
2016-02-03 15:06:30 -0500

    added extra methods

--- src/ATM.java
+++ src/ATM.java
@@ -2,13 +2,15 @@
 public class ATM extends Currency 
 {
     int moneyDeposit;
+    int moneyWithdraw;
     
-    public ATM(int money,int conversion)
+    public ATM(int money,int conversion,int moneyDeposit, int moneyWithdraw)
     {
         super(money, conversion);
         this.moneyDeposit= moneyDeposit;
+        this.moneyWithdraw=moneyWithdraw;
     }
-     public int WithrawalMoney()
+     public int DepositedMoney()
      {
          return money = DepositMoney() + money;
      }
@@ -16,4 +18,13 @@ public class ATM extends Currency
     {
         return moneyDeposit;
     }
+    public int WithdrawMoney()        
+    {
+        return money = WithdrawalAmount() - money;
+    }
+    public int WithdrawalAmount()
+    {
+        return moneyDeposit;
+    }
+    
 }
